f00f782b9cf32ffacab3212265edf224558706ac
xuxiaofei820825 <flying.xu77@gmail.com>
2017-08-03 11:15:24 +0800

    Add files via upload

---
+++ src/main/java/com/github/binary2bean/annotation/BCDCode.java
@@ -0,0 +1,15 @@
+package com.github.binary2bean.annotation;
+
+import static java.lang.annotation.ElementType.FIELD;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(FIELD)
+@Documented
+public @interface BCDCode {
+	int bytes() default 0;
+}
